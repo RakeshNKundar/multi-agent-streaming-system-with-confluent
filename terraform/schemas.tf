d73a49a8766459520bf35c20e10bafaac5841e21
vdeshpande-confluent <148322451+vdeshpande-confluent@users.noreply.github.com>
2025-09-24 08:47:51 +0530

    added mongo-schema

--- terraform/schemas.tf
+++ terraform/schemas.tf
@@ -36,3 +36,22 @@ resource "confluent_schema" "scheduler_agent_response" {
   }
 }
 
+resource "confluent_schema" "mongo_agent_response" {
+  schema_registry_cluster {
+    id = data.confluent_schema_registry_cluster.default.id
+  }
+  rest_endpoint      = data.confluent_schema_registry_cluster.default.rest_endpoint
+  subject_name       = "mongo_agent_response-value"
+  format             = "AVRO"
+  schema             = file("../schemas/mongo_agent_response.avsc")
+  recreate_on_update = false
+  hard_delete        = true
+  credentials {
+    key    = confluent_api_key.schema-registry-api-key.id
+    secret = confluent_api_key.schema-registry-api-key.secret
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
